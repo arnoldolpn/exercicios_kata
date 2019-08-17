039e8e4ef86b698d95aaa83b5994ae6155437be2
arnoldolpn <arnoldoliesenberg@gmailcom>
2019-08-17 16:04:45 -0300

    comitando os exercicios 0, 1, 2, 3 e 4

--- 0-index.test.js
+++ 0-index.test.js
@@ -1,10 +1,11 @@
 function hello(name) {
-  // IMPLEMENTE
-}
+  return `Hello, ${name}!`;
+  }
 
 describe('hello', function() {
   it('deve retornar Hello, Alan!', function() {
     expect(hello('Alan')).toBe('Hello, Alan!');
   });
 });
-  
+
+
--- 3-functions.test.js
+++ 3-functions.test.js
@@ -1,8 +1,9 @@
 describe('Funções', function() {
   it('Implemente uma função chamada add que recebe dois parâmtros e retorna a soma deles', function() {
-
-    // IMPLEMENTE
-
+    var add = function(x, y){
+      return x + y;
+    }
+    
     expect(add(1, 2)).toBe(3);
   });
 });
