b8a571b018948faac428928c676fe61a2a36d75f
arnoldolpn <arnoldoliesenberg@gmailcom>
2019-08-29 22:20:06 -0300

    subindo ex6

--- 6-object-oriented.test.js
+++ 6-object-oriented.test.js
@@ -1,6 +1,17 @@
 
 describe('Orientação a Objetos', function() {
 
+  class Pessoa 
+  {
+    constructor(firstname, lastname){
+      this.firstname = firstname
+      this.lastname = lastname
+    }
+    fullname(){
+      return this.firstname + " " + this.lastname
+    }
+  }
+  
   // IMPLEMENTE a classe Pessoa que recebe dois parâmetros: firstname e lastname no construtor
   // e implemente o método fullname() que retorna o nome completo
 
